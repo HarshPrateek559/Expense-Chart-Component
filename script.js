c79c810f1d54bc5cd4acb553efbb111e1040aeef
HarshPrateek559 <neutronspace@gmail.com>
2022-05-31 13:04:36 +0530

    completed the project

---
+++ script.js
@@ -0,0 +1,26 @@
+//Database to give heights to the bars.
+const data = [17.45, 34.91, 52.36, 31.07, 23.39, 43.28, 25.48];
+
+let bars = document.getElementsByClassName("bar");
+let graph = document.getElementsByClassName("graph");
+
+//setting the height of the bars
+let i = 0;
+while (i < 7) {
+  bars[i].style.height = `${1.5 * data[i]}%`;
+  i++;
+}
+
+Array.from(bars).forEach((e) => {
+  e.addEventListener("mouseover", () => {
+    let div = document.createElement("p");
+    div.classList.add("show");
+    let text = document.createTextNode(`$${data[e.id - 1]}`);
+    div.appendChild(text);
+    graph[e.id - 1].appendChild(div);
+  });
+  e.addEventListener("mouseout", () => {
+    let rem = document.querySelector(".show");
+    rem.remove();
+  });
+});
